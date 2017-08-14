dc9737601d4bb6142c994ca42f12cb85634ad0ba
Devansh Jain <j.devansh29@gmail.com>
2017-08-14 16:32:58 -0700

    complete working scripts for tweet scraping

--- countinous_api_scraper_tesla.py
+++ countinous_api_scraper_tesla.py
@@ -9,9 +9,7 @@ import pdb
 
 # connects to the mongo server, defines a database twitter, and then a collection (table)
 client = MongoClient()
-db = client.twitter
-collection_name = 'tweets'
-collection = db.collection_name
+collection = client.twitter_db.tesla
 
 # these lines are setting api keys from api_keys.txt.
 with open ("api_keys.txt", "r") as myfile:
@@ -22,33 +20,22 @@ consumer_key , consumer_secret , access_token , access_secret = data[0].strip(),
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
+pdb.set_trace()
 
-max_id = 896188912883777537
-# current id at 6:58pm on Friday 11th August 2017
-x = 0
+if collection.count() == 0:
+    max_id = 896188912883777537
+    # current id at 6:58pm on Friday 11th August 2017
+else:
+    max_id = collection.find()[collection.count()-1]['id']
 
-while x < 6:
-    results = api.search('earthquake', count=5, max_id = max_id)
+pdb.set_trace()
+
+
+while True:
+    results = api.search('#tesla', count=100, max_id = max_id)
     for result in results:
-            # print ('')
-            # if result.lang == 'en' and result.retweeted and result.favorited and result.user.followers_count > 2000:
             if result.lang == 'en' and result.user.followers_count > 100:
                 collection.insert_one(result._json)
-                print (result._json['id'])
                 print (result._json['text'])
-                print (result._json['lang'])
-
-                # print (max_id,collection.count())
-                # pdb.set_trace()
-
-    max_id = collection.find()[collection.count()-1]['id'] - 1
-    print (max_id,collection.count())
-    x += 1
-    # for x in range(collection.count()):
-    #     print (collection.find()[x]['text'])
-
-    # break
-
-# print (collection.count())
-# prints the whole collection as json
-# print(dumps(collection.find()))
+                print (max_id,collection.count())
+    max_id = collection.find()[collection.count()-1]['id']
