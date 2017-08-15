c25f28e8c3c7dbf2bfeeb98832a230946cd1bfbc
Devansh Jain <j.devansh29@gmail.com>
2017-08-14 17:11:14 -0700

    add working script that returns a gcloud sentimentResponse for input text

---
+++ sentiment_analyzer_google_cloudapi.py
@@ -0,0 +1,15 @@
+#run gcloud_api_key_path.txt after every reboot,
+from google.cloud import language
+import pdb
+
+# takes text as input, returns a sentiment analysis object from google cloud api.
+# call .score and .magnitude on that object to get those two results for the input text.
+def language_analysis(text):
+    client = language.Client()
+    document = client.document_from_text(text)
+    return document.analyze_sentiment().sentiment
+
+
+example_text = 'I love data science!'
+sentiment = language_analysis(example_text)
+print('score:',sentiment.score,'magnitude:', sentiment.magnitude)
